7dbe85386e552f1a1ac37c0c9afd6bffbd5c262f
Valter Silva <valterh@amazon.com>
2022-08-15 22:14:26 +0000

    Organize imports

--- app/doc.py
+++ app/doc.py
@@ -13,11 +13,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-
 import pandas as pd
-
 import logging
-
 import config as _config
 
 log = logging.getLogger("aws-auto-inventory.doc")
